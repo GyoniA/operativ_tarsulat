487dba32c93aed6843227046a66eb26d864e626c
GyoniA <gyoniakos@gmail.com>
2022-03-23 11:23:10 +0100

    Néhány bent maradt varázsló és ereklye kiszedése, az ágensek és genetikus kódok egy részének implementálása.

--- src/com/operativ_tarsulat/AmnesiaGeneticCode.java
+++ src/com/operativ_tarsulat/AmnesiaGeneticCode.java
@@ -1,20 +1,11 @@
 package com.operativ_tarsulat;
 
 public class AmnesiaGeneticCode extends GeneticCode {
-	@Override
     public  AmnesiaVirus CreateInstance(Virologist v) {
 		throw new RuntimeException("Unimplemented");
     }
     
-    public int GetAminoCost() {
-    	throw new RuntimeException("Unimplemented");
-    }
-    
     public void CheckList(GeneticCodeCheckList ls) {
-    	throw new RuntimeException("Unimplemented");
-    }
-    
-    public int GetNucleoCost() {
-    	throw new RuntimeException("Unimplemented");
+    	ls.Amnesia = true;
     }
 }
