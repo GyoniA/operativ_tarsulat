8e6827585263c0b9129e01d9d3d8362d3abfef16
jozsi01 <bozsikjozsi01@gmail.com>
2022-04-22 13:09:54 +0200

    Testhez szukseges változás

--- src/com/operativ_tarsulat/Tests.java
+++ src/com/operativ_tarsulat/Tests.java
@@ -1,10 +1,56 @@
 package com.operativ_tarsulat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Tests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			System.out.println("Adja meg a parancsot!");
+			String command = "";
+			try {
+				command = br.readLine();
+			}catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+			switch(command) {
+			case "loadGame": 
+				LoadGameTest();
+				break;
+			case "saveGame":
+				SaveGameTest();
+				break;
+			default: System.out.println("Rossz parancs!");
+			}
+			
+		}
+		
+		
 	}
-
+	
+	static void LoadGameTest()
+	{		
+		BufferedReader lr = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Játék neve: ");
+		String gameName = "";
+		try {
+			gameName = lr.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Game.GetInstance().LoadGame(gameName);
+	}
+	
+	static void SaveGameTest()
+	{
+		
+	}
+	
+	
 }
