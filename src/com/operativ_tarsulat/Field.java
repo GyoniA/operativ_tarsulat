aafb1a9118b1b7da054705e6abf2646815ae8901
GyoniA <gyoniakos@gmail.com>
2022-04-22 11:59:28 +0200

    Field  comments.

--- src/com/operativ_tarsulat/Field.java
+++ src/com/operativ_tarsulat/Field.java
@@ -9,19 +9,33 @@ public abstract class Field implements Serializable {
     private LinkedList<Virologist> virologists;
     private List<Field> neighbours;
     public Gear droppedGear;
+
+	/**
+	 * Constructor to create a new field
+	 */
     public Field() {
     	Skeleton.LogFunctionCall("Field ctr");
     	neighbours = new LinkedList<Field>();
     	virologists = new LinkedList<Virologist>();
     	Skeleton.LogReturn();
     }
-    
+
+	/**
+	 * Adds a virologist to this field
+	 * @param v the virologist to add
+	 */
 	public void Accept(Virologist v) {
     	Skeleton.LogFunctionCall(new Object() {}.getClass().getEnclosingMethod().getName(),v.getName(),v.getClass().getName());
     	virologists.add(v);
     	Skeleton.LogReturn();
     }
-    
+
+	/**
+	 * Removes a virologist from one of this field's neighbours
+	 * @param v the virologist to remove
+	 * @param f the field to remove the virologist from
+	 * @return true if the virologist was removed, false otherwise
+	 */
     public Boolean Remove(Virologist v, Field f) {
     	Skeleton.LogFunctionCall(new Object() {}.getClass().getEnclosingMethod().getName(),v.getName(),f.getClass().getName(),v.getClass().getName());
     	if(neighbours.contains(f))
@@ -36,23 +50,39 @@ public abstract class Field implements Serializable {
     }
     
     public abstract void Interact(Virologist v);
-    
-    public List<Field> GetNeighbours() {
+
+	/**
+	 * Returns the list of neighbours of this field
+	 * @return the neighbours
+	 */
+	public List<Field> GetNeighbours() {
     	Skeleton.LogFunctionCall(new Object() {}.getClass().getEnclosingMethod().getName());
     	Skeleton.LogReturn(neighbours.toString());
     	return neighbours;
     }
-    
-    public void AddNeighbour(Field f) {
+
+	/**
+	 * Adds a neighbour to this field
+	 * @param f the neighbour to add
+	 */
+	public void AddNeighbour(Field f) {
     	Skeleton.LogFunctionCall(new Object() {}.getClass().getEnclosingMethod().getName());
     	Skeleton.LogReturn();
     	neighbours.add(f);
     }
 
+	/**
+	 * Returns the list of virologists in this field
+	 * @return the virologists
+	 */
 	public Virologist[] GetVirologists() {
 		return virologists.toArray(new Virologist[virologists.size()]);
 	}
 
+	/**
+	 * Removes a virologist from this field
+	 * @param v the virologist to remove
+	 */
 	public void Remove(Virologist v) {
 		virologists.remove(v);
 	}
