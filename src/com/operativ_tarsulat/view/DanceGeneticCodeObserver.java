bfb0d6e234bc54c99c2e2bf2b949447a255b9a5d
GyoniA <gyoniakos@gmail.com>
2022-05-13 10:28:05 +0200

    More observer definitions.

---
+++ src/com/operativ_tarsulat/view/DanceGeneticCodeObserver.java
@@ -0,0 +1,11 @@
+package com.operativ_tarsulat.view;
+
+import com.operativ_tarsulat.model.DanceGeneticCode;
+
+public class DanceGeneticCodeObserver implements Observer {
+    DanceGeneticCode subject;
+
+    public void Update(){
+
+    }
+}
---
+++ src/com/operativ_tarsulat/view/ProtectionGeneticCodeObserver.java
@@ -0,0 +1,11 @@
+package com.operativ_tarsulat.view;
+
+import com.operativ_tarsulat.model.ProtectionGeneticCode;
+
+public class ProtectionGeneticCodeObserver implements Observer {
+    ProtectionGeneticCode subject;
+
+    public void Update(){
+
+    }
+}
